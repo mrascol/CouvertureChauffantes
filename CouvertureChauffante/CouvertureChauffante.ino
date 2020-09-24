77315bbe6a45c051c518c1dfdb178713227824bc
Mathias <mathias.rascol@gmail.com>
2020-09-24 22:55:36 +0200

    Suppression du mode expert

--- CouvertureChauffante/CouvertureChauffante.ino
+++ CouvertureChauffante/CouvertureChauffante.ino
@@ -23,7 +23,7 @@
 
 // VERSION
 String hwVersion="1.0";
-String swVersion="1.1";
+String swVersion="1.2";
 
 // Debug MODE
 bool dbgMode = 1;
@@ -57,11 +57,6 @@ int consigne[2]={50,50};
 int correctionTemp[4]={0,0,0,0};
 int temperature[4]={0,0,0,0};
 
-//Conf de l'expertMode
-byte expertModeNbStep=3;
-int expertModeConsigneFront[3]={10, 20, 30};
-int expertModeConsigneRear[3]={15, 25, 35};
-int expertModeStepLength[3]={5,10,15};  //Si la valeur vaut 61, on affichera "OFF"
 
 //Initialisation des capteurs de temp
 int sensorFL=A1;
@@ -81,10 +76,6 @@ unsigned int consigneEepromAddress[2] = {0,1};
 unsigned int autoCutValEepromAddress = 4;
 unsigned int screenContrastValEepromAddress = 5;
 unsigned int correctionTempEepromAddress[4] = {6,7,8,9};
-unsigned int expertModeNbStepEepromAddress = 10;
-unsigned int expertModeConsigneFrontEepromAddress[3]={11, 12, 13};
-unsigned int expertModeConsigneRearEepromAddress[3]={14, 15, 16};
-unsigned int expertModeStepLengthEepromAddress[3]={17,18,19}; 
 
 // Création du caractère Flèche
 byte arrow[8] = {
@@ -172,19 +163,6 @@ void setup() {
   correctionTemp[2] = readEEPROM(eeprom, correctionTempEepromAddress[2]);
   correctionTemp[3] = readEEPROM(eeprom, correctionTempEepromAddress[3]);   
 
-  expertModeNbStep= readEEPROM(eeprom, expertModeNbStepEepromAddress);
-
-  expertModeConsigneFront[0]= readEEPROM(eeprom, expertModeConsigneFrontEepromAddress[0]);
-  expertModeConsigneFront[1]= readEEPROM(eeprom, expertModeConsigneFrontEepromAddress[1]);
-  expertModeConsigneFront[2]= readEEPROM(eeprom, expertModeConsigneFrontEepromAddress[2]);
-
-  expertModeConsigneRear[0]= readEEPROM(eeprom, expertModeConsigneRearEepromAddress[0]);
-  expertModeConsigneRear[1]= readEEPROM(eeprom, expertModeConsigneRearEepromAddress[1]);
-  expertModeConsigneRear[2]= readEEPROM(eeprom, expertModeConsigneRearEepromAddress[2]);
-
-  expertModeStepLength[0]= readEEPROM(eeprom, expertModeStepLengthEepromAddress[0]);
-  expertModeStepLength[1]= readEEPROM(eeprom, expertModeStepLengthEepromAddress[1]);
-  expertModeStepLength[2]= readEEPROM(eeprom, expertModeStepLengthEepromAddress[2]);
 
   //Mise jour du contrast
   analogWrite(screenContrastPin, screenContrastLst[screenContrastVal]);
@@ -209,10 +187,9 @@ void loop() {
   int posMenuNew=0;
   
   //Menu Construction
-  String mainMenuLib[3];
+  String mainMenuLib[2];
   mainMenuLib[0]= "1.Quick Warming";
-  mainMenuLib[1]= "2.Expert Mode";
-  mainMenuLib[2]= "3.Setup";
+  mainMenuLib[1]= "2.Setup";
 
   //On affiche le premier Menu
   lcd.clear();
@@ -221,7 +198,7 @@ void loop() {
 
   while ((1)){
     // On attend qu'un bouton soit pressé
-    posMenuNew = readBtn(posMenu, 0, 3);
+    posMenuNew = readBtn(posMenu, 0, 2);
   
     if (dbgMode>=1){Serial.println(fctName+F("|posMenuNew=")+String(posMenuNew));}
     
@@ -239,9 +216,6 @@ void loop() {
           warmingMenu();
           break;
         case 1 :
-          expertModeMenu();
-          break;
-        case 2 :
           setupMenu();
           break;
       }
@@ -364,81 +338,6 @@ void warmingMenu(){
 }
 
 
-// Fonction qui permet de faire la chauffe 
-// IN : Le Step dans lequel on est (en mode Expert) 
-// OUT : la valeur de la touche presse (-2 = Back / -1 = Valid)
-int warmingMenuExpert(byte currentStep){
-  String fctName="warmingMenuExpert";
-
-  int posMenu=0;
-  int posMenuNew=0;
-  bool keepWarming=1;
-  
-  //Menu Construction
-  String menuLib[2];
-  unsigned long startWarmingTime;
-  unsigned long remainingTime;
-
-  startWarmingTime=millis();
-  //On initialise l'affichage
-  menuLib[0]= "Stp"+ String(currentStep+1) + " FL=__ R=__";
-  menuLib[1]= "___m FL=__ R=__";
-  posMenu=posMenuNew;
-  lcd.clear();
-  lcd.noCursor();
-  lcd.noBlink();
-  
-  while ((keepWarming==1)){
-    // On check la température et on ajuste la tension qu'on pousse sur chaque couverture
-    //1. read les 4 temps
-    //2. ajuster la tension
-    //3. Ajuster l'affichage
-    lcd.setCursor(0,0);
-    lcd.print(menuLib[0]);
-    lcd.setCursor(0,1);
-    lcd.print(menuLib[1]);
-    if (expertModeStepLength[currentStep]> 60){
-      //affichage logo infini
-      lcd.setCursor(0,1);
-      lcd.print(F(" "));
-      lcd.setCursor(1,1);
-      lcd.write(byte(1));
-      lcd.setCursor(2,1);
-      lcd.write(byte(1));
-    }
-
-    
-    temperature[0]=warmingCheckAdjust(sensorFL, chauffeFL, expertModeConsigneFront[currentStep], correctionTemp[0], 0, 17);
-    temperature[1]=warmingCheckAdjust(sensorFR, chauffeFR, expertModeConsigneFront[currentStep], correctionTemp[1], 0, 17);
-    temperature[2]=warmingCheckAdjust(sensorRL, chauffeRL, expertModeConsigneRear[currentStep], correctionTemp[2], 0, 17);
-    temperature[3]=warmingCheckAdjust(sensorRR, chauffeRR, expertModeConsigneRear[currentStep], correctionTemp[3], 0, 17);
-    
-    //On met à jour l'affichage
-    //Duree du Step - (heure courante -heure de début)
-    remainingTime=expertModeConsigneFront[currentStep]-(round(millis()-startWarmingTime)/1000/60);    
-    menuLib[0]= "Stp"+ String(currentStep+1) + " FL="+String((round(temperature[0])))+" FR="+String((round(temperature[1])));
-    menuLib[1]= String(padding(remainingTime,3))+"m" + " FL="+ String((round(temperature[2])))+" RR="+String((round(temperature[3])));
-
-    // On attend qu'un bouton soit pressé
-    posMenu=0;
-    posMenuNew = readBtn(posMenu, 0, 2);
-    
-    // si la touche back est pressee, alors on revient à l'écran d'avant
-    // si la touche valid est pressee, alors on passe au step suivant
-    // Dans tous les cas on retourne cette valeure
-    if (posMenuNew < 0 ){
-      keepWarming=0;
-    }
-  }
-  //On coupe la chauffe avant de sortir
-  digitalWrite(chauffeFL, LOW);
-  digitalWrite(chauffeFR, LOW);
-  digitalWrite(chauffeRL, LOW);
-  digitalWrite(chauffeRR, LOW);
-  return posMenuNew;
-
-  
-}
 
 // Fonction qui permet de régler les consignes de température
 // IN : le port du Sensor, la consigne pour ce port, la ligne pour l'affichage, la colonne pour l'affichage
@@ -653,270 +552,6 @@ void setupMenu(){
   }
 }
 
-
-// Fonction du Menu du mode Expert
-// IN : N/A
-// OUT : N/A
-void expertModeMenu(){
-  String fctName="expertModeMenu";
-
-  int posMenu=0;
-  int posMenuNew=0;
-  int returnedValue;
-  bool keepStepping=1;
-  byte i=0;
-  bool keepMenu=1;
-  //Menu Construction
-  String menuLib[2][2];
-  menuLib[0][0]= {"1.Expert Warming"};
-  menuLib[0][1]= {""};
-  menuLib[1][0]= {"2.Expert Setup"};
-  menuLib[1][1]= {""};
- 
-  //On affiche le premier Menu
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(menuLib[posMenu][0]);
-  lcd.setCursor(0,1);
-  lcd.print(menuLib[posMenu][1]);
-
-  while ((keepMenu==1)){
-    // On attend qu'un bouton soit pressé
-    posMenuNew = readBtn(posMenu, 0, 2);
-  
-    if (dbgMode>=1){Serial.println(fctName+F("|posMenuNew=")+String(posMenuNew));}
-    
-    // Si la touche Valide est pressee, alors on passe dans le menu suivant
-    if (posMenuNew == -1){
-      switch (posMenu){
-        case 0 : //Lancer le mode Step
-          //On va parcourir les Steps du mode expert et on va appliquer les consignes sur la durée demandée
-          //La touche Back va arreter la chauffe
-          //La touche Up va permettre de changer les temps avec sauvegarde dans l'EEPROM
-          i=0;
-          keepStepping=1;
-          while (i<expertModeNbStep && keepStepping==1){
-            returnedValue = warmingMenuExpert(i);
-            if (returnedValue == -2){
-              //La touche a été pressée, alors on sort sans aller au bout
-              keepStepping=0;
-            }
-            i++;
-          }
-          break;
-        case 1 : //Config des step
-          expertModeSetup();
-          break;
-      }
-
-      posMenuNew=posMenu;
-    }
-    // si la touche back est pressee, alors on revient à l'écran d'avant
-    if (posMenuNew ==-2){
-      keepMenu=0;;
-      posMenuNew=posMenu;
-    }
-    
-    posMenu=posMenuNew;
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print(menuLib[posMenu][0]);
-    lcd.setCursor(0,1);
-    lcd.print(menuLib[posMenu][1]);
-  }
-}
-
-
-
-// Fonction de setup en mode expert
-// IN : N/A
-// OUT : N/A
-void expertModeSetup(){
-  String fctName="expertModeSetup";  
-  int posMenu=0;
-  int posMenuNew=0;
-  bool keepMenu=1;
-
-
-  //Menu Construction
-  String menuLib[4][2];
-  menuLib[0][0]= "Nb Steps";
-  menuLib[0][1]= String(expertModeNbStep);
-  menuLib[1][0]= "Step1";
-  menuLib[1][1]= String(padding(expertModeStepLength[0],2))+ "min F=" + String(padding(expertModeConsigneFront[0],2)) + " R=" + String(padding(expertModeConsigneRear[0], 2));
-  menuLib[2][0]= "Step2";
-  menuLib[2][1]= String(padding(expertModeStepLength[1],2))+ "min F=" + String(padding(expertModeConsigneFront[1],2)) + " R=" + String(padding(expertModeConsigneRear[1], 2));
-  menuLib[3][0]= "Step3";
-  menuLib[3][1]= String(padding(expertModeStepLength[2],2))+ "min F=" + String(padding(expertModeConsigneFront[2],2)) + " R=" + String(padding(expertModeConsigneRear[2], 2));
-  
-  //On affiche le premier Menu
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(menuLib[posMenu][0]);
-  lcd.setCursor(0,1);
-  lcd.print(menuLib[posMenu][1]);
-
-  if (expertModeStepLength[posMenu]> 60){
-      //affichage logo infini
-      lcd.setCursor(0,1);
-      lcd.write(byte(1));
-      lcd.setCursor(1,1);
-      lcd.write(byte(1));
-    }
-
-    while ((keepMenu==1)){
-    // On attend qu'un bouton soit pressé
-    posMenuNew = readBtn(posMenu, 0, expertModeNbStep+1);
-        
-    // Si la touche Valide est pressee, alors on passe dans le menu suivant
-    if (posMenuNew == -1){
-      if (posMenu==0){
-        // Config du nb de Step
-        expertModeSetupNbStep();
-        menuLib[0][1]= String(expertModeNbStep);
-      }
-      else {
-        // Config du step ciblé
-        expertModeSetupStep(posMenu-1);
-        menuLib[posMenu][1]= String(padding(expertModeStepLength[posMenu],2))+F("min F=") + String(padding(expertModeConsigneFront[posMenu],2)) + F(" R=") + String(padding(expertModeConsigneRear[posMenu], 2));
-      }
-
-      posMenuNew=posMenu;
-    }
-    // si la touche back est pressee, alors on revient à l'écran d'avant
-    if (posMenuNew ==-2){
-      keepMenu=0;;
-      posMenuNew=posMenu;
-    }
-    
-    posMenu=posMenuNew;
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print(menuLib[posMenu][0]);
-    lcd.setCursor(0,1);
-    lcd.print(menuLib[posMenu][1]);
-    
-    if (expertModeStepLength[posMenu]> 60){
-      //affichage logo infini
-      lcd.setCursor(0,1);
-      lcd.write(byte(1));
-      lcd.setCursor(1,1);
-      lcd.write(byte(1));
-    }
-  }
-}
-
-// Fonction du config du nombre de Step mode Expert
-// IN : N/A
-// OUT : N/A
-void expertModeSetupNbStep(){
-  String fctName="expertModeSetupNbStep";
-  
-  int posMenuNew=0;
-  bool keepMenu=1;
-
-  lcd.setCursor(0,1);
-  lcd.cursor();
-  lcd.blink();
-    
-  while(keepMenu==1){
-    posMenuNew = readBtn(expertModeNbStep, 1, 4);
-    if (posMenuNew ==-1 || posMenuNew==-2){
-      posMenuNew=expertModeNbStep;
-      keepMenu=0;
-    }
-    else{
-      expertModeNbStep=posMenuNew;
-    }
-    lcd.print(String(expertModeNbStep));
-    lcd.setCursor(0,1);
-  }
-
-  //Avant de sortir, on enregistre dans l'EEPROM
-  //writeEEPROM(eeprom, screenContrastValEepromAddress, screenContrastVal);
-  
-  lcd.noCursor();
-  lcd.noBlink();    
-}
-
-// Fonction du config d'un Step mode Expert
-// IN : N/A
-// OUT : N/A
-void expertModeSetupStep(int monStep){
-  String fctName="expertModeSetupStep";
-
-  int posMenu=0;
-  int posMenuNew=0;
-  int itemEnCours=0; //0 : c'est le délai, 1 : c'est la temp avant, 2: c'est la temps arrière
-  int itemScreenPos[3]={0, 8, 13};
-  bool keepMenu=1;
-
-  lcd.cursor();
-  lcd.blink();
-    
-  while(keepMenu==1){
-    //On positionne le cursor au bon endroit
-    lcd.setCursor(itemScreenPos[itemEnCours],1);
-    
-    //la valeur max dépend de l'item qu'on est en train de paramatrer
-    // Pour la durée du Step : 5 à 60 et OFF
-    // Pour les température : 20 à 75
-
-    if (itemEnCours ==0 ){
-        posMenu = expertModeStepLength[monStep];
-        posMenuNew = readBtn(posMenu, 2, 62);
-    }
-    else {
-      if (itemEnCours ==1){
-        posMenu = expertModeConsigneFront[monStep];
-        posMenuNew = readBtn(posMenu, 20, 75);
-      }
-      else {
-        posMenu = expertModeConsigneRear[monStep];
-        posMenuNew = readBtn(posMenu, 20, 75);
-      }
-    }
-    
-    if (posMenuNew ==-2 ){
-      //si back est pressé on sort
-      keepMenu=0;
-    } else {
-      if (posMenuNew==-1){
-        // si val est pressé on passe à la valeur suivante et on déplace le curseur
-        itemEnCours++;
-      }
-      else{
-        // Sinon on met à jour la valeur en cours
-        // dans la variable et sur le LCD
-        // specificité pour délai qui peut être mis à off si = 61min
-        if (itemEnCours ==0 ){
-          expertModeStepLength[monStep]=posMenuNew;   
-        }
-        else {
-          expertModeConsigneFront[monStep]=posMenuNew;
-        }    
-      }
-    }
-
-    //MAJ affichage
-    lcd.setCursor(0,1);
-      lcd.print(String(padding(expertModeStepLength[monStep],2))+F("min F=") + String(padding(expertModeConsigneFront[monStep],2)) + F(" R=") + String(padding(expertModeConsigneRear[monStep], 2)));
-    if (expertModeStepLength[monStep]> 60){
-      //affichage logo infini
-      lcd.setCursor(0,1);
-      lcd.write(byte(1));
-      lcd.setCursor(1,1);
-      lcd.write(byte(1));
-    }
-  }
-
-  //Avant de sortir, on enregistre dans l'EEPROM
-  //writeEEPROM(eeprom, screenContrastValEepromAddress, screenContrastVal);
-  
-  lcd.noCursor();
-  lcd.noBlink();  
-}
-
 // Fonction de paramétrage du contrast
 // IN : N/A
 // OUT : N/A
@@ -1129,20 +764,6 @@ void restoreDefault(){
       writeEEPROM(eeprom, correctionTempEepromAddress[2], 0);
       writeEEPROM(eeprom, correctionTempEepromAddress[3], 0);
 
-      //Variables pour le mode expert
-      writeEEPROM(eeprom, expertModeNbStepEepromAddress, 3);
-      writeEEPROM(eeprom, expertModeConsigneFrontEepromAddress[0], 50);
-      writeEEPROM(eeprom, expertModeConsigneFrontEepromAddress[1], 55);
-      writeEEPROM(eeprom, expertModeConsigneFrontEepromAddress[2], 60);
-
-      writeEEPROM(eeprom, expertModeConsigneRearEepromAddress[0], 50);
-      writeEEPROM(eeprom, expertModeConsigneRearEepromAddress[1], 55);
-      writeEEPROM(eeprom, expertModeConsigneRearEepromAddress[2], 60);
-
-      writeEEPROM(eeprom, expertModeStepLengthEepromAddress[0], 20);
-      writeEEPROM(eeprom, expertModeStepLengthEepromAddress[1], 5);
-      writeEEPROM(eeprom, expertModeStepLengthEepromAddress[2], 10);
-
       // On va aller Lire le contenu de l'EEPROM
       consigne[0] = readEEPROM(eeprom, consigneEepromAddress[0]);
       consigne[1] = readEEPROM(eeprom, consigneEepromAddress[1]);
@@ -1154,19 +775,6 @@ void restoreDefault(){
       correctionTemp[2] = readEEPROM(eeprom, correctionTempEepromAddress[2]);
       correctionTemp[3] = readEEPROM(eeprom, correctionTempEepromAddress[3]);
 
-      expertModeNbStep= readEEPROM(eeprom, expertModeNbStepEepromAddress);
-
-      expertModeConsigneFront[0]= readEEPROM(eeprom, expertModeConsigneFrontEepromAddress[0]);
-      expertModeConsigneFront[1]= readEEPROM(eeprom, expertModeConsigneFrontEepromAddress[1]);
-      expertModeConsigneFront[2]= readEEPROM(eeprom, expertModeConsigneFrontEepromAddress[2]);
-
-      expertModeConsigneRear[0]= readEEPROM(eeprom, expertModeConsigneRearEepromAddress[0]);
-      expertModeConsigneRear[1]= readEEPROM(eeprom, expertModeConsigneRearEepromAddress[1]);
-      expertModeConsigneRear[2]= readEEPROM(eeprom, expertModeConsigneRearEepromAddress[2]);
-
-      expertModeStepLength[0]= readEEPROM(eeprom, expertModeStepLengthEepromAddress[0]);
-      expertModeStepLength[1]= readEEPROM(eeprom, expertModeStepLengthEepromAddress[1]);
-      expertModeStepLength[2]= readEEPROM(eeprom, expertModeStepLengthEepromAddress[2]);
                
       delay(1500);
       keepMenu=0;
