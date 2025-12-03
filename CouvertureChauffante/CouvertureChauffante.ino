9e63b83444d5b4a26dd4e38e860ac6cbb4554fd0
Mathias <mathias.rascol@gmail.com>
2025-12-03 23:51:23 +0100

    V4.0 - Passage avec TO220 (TIP¨120)

--- CouvertureChauffante/CouvertureChauffante.ino
+++ CouvertureChauffante/CouvertureChauffante.ino
@@ -9,9 +9,10 @@
 #include <LiquidCrystal_I2C.h>
 
 // Version
-//const String hVersion="HW=2.0    SW=3.6";
-const String hVersion="HW=3.0    SW=3.7";
-//const String hVersion="HW=3.1    SW=3.6";
+const String hVersion="HW=2.0    SW=3.7";
+//const String hVersion="HW=3.0    SW=3.7";
+//const String hVersion="HW=3.1    SW=3.7";
+//const String hVersion="HW=4.0    SW=3.7";
 
 
 
